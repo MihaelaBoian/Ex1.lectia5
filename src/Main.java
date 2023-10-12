cf74b928a171536188e17fbad9def80b14e985f7
MihaelaBoian <pinzari_m@mail.ru>
2023-10-12 16:21:18 +0300

    j

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int pozitiv = 0;
+        int negativ = 0;
+        int suma = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+         int g =  scanner.nextInt();
+         if (g == 0) {
+             System.out.println("Exit");
+             break;
+         }
+
+         if (g > 0) {
+             pozitiv++;
+             System.out.println("Numarul introdus la consola este: " + g +  " care e pozitiv");
+         } else {
+             negativ++;
+             System.out.println("Numarul introdus la consola este: " + g +  " care e negativ");
+         }
+
+         suma = suma + g;
+
+       }
+
+        System.out.println("Suma este: " + suma);
+        System.out.println("Numere pozitive sunt: " + pozitiv);
+        System.out.println("Numere negative sunt: " + negativ);
+
     }
 }
